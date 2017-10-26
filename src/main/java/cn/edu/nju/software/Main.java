135acd3d22b1515ef04e2af4c26392f6515bfd15
dalecgu <1677160278@qq.com>
2017-10-26 20:18:41 +0800

    update

--- src/main/java/cn/edu/nju/software/Main.java
+++ src/main/java/cn/edu/nju/software/Main.java
@@ -1,7 +1,9 @@
 package cn.edu.nju.software;
 
+import cn.edu.nju.software.entity.Comment;
 import cn.edu.nju.software.service.NlpService;
 import cn.edu.nju.software.service.NlpServiceImpl;
+import com.alibaba.fastjson.JSON;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -14,12 +16,18 @@ public class Main {
 
         try (BufferedReader reader = new BufferedReader(new FileReader("comments.json"));
              NlpService service = new NlpServiceImpl()) {
-            StringBuilder content = new StringBuilder();
             String line;
+            int i = 0;
+            String content = "";
             while (null != (line = reader.readLine())) {
-                content.append(line);
+                Comment comment = JSON.parseObject(line, Comment.class);
+                content = content.concat(comment.getContent());
+                i++;
+                if (i == 1000) {
+                    System.out.println(service.getKeyWords(content, 10));
+                    i = 0;
+                }
             }
-            System.out.println(service.getKeyWords(content.toString(), 10));
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ src/main/java/cn/edu/nju/software/entity/Comment.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.software.entity;
+
+public class Comment {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
