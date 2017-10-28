5366d721363c4a2ab6291a091a9976feec59d811
dalecgu <1677160278@qq.com>
2017-10-29 02:08:59 +0800

    update

--- src/main/java/cn/edu/nju/software/spark/JavaWordCount.java
+++ src/main/java/cn/edu/nju/software/spark/JavaWordCount.java
@@ -3,36 +3,8 @@ package cn.edu.nju.software.spark;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Pattern;
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
-import scala.Tuple2;
-
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.sql.SparkSession;
 
 import java.util.Arrays;
 import java.util.List;
@@ -48,13 +20,13 @@ public final class JavaWordCount {
             System.exit(1);
         }
 
-        SparkSession spark = SparkSession
-                .builder()
-                .master("spark://192.168.1.112:7077")
-                .appName("JavaWordCount")
-                .getOrCreate();
+        SparkConf conf = new SparkConf()
+                .setAppName("JavaWordCount")
+                .setMaster("spark://192.168.1.112:7077")
+                .setJars(new String[] {"/Users/dalec/IdeaProjects/ictclas/target/ictclas-0.1-jar-with-dependencies.jar"});
+        JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
+        JavaRDD<String> lines = sc.textFile(args[0]);
 
         JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
 
@@ -66,6 +38,6 @@ public final class JavaWordCount {
         for (Tuple2<?,?> tuple : output) {
             System.out.println(tuple._1() + ": " + tuple._2());
         }
-        spark.stop();
+        sc.stop();
     }
 }
